d2e02ab9f8e0c7657b23b59b17fef0e1a146eec6
Aryosmar <aryfelipe95@hotmail.com>
2024-11-09 16:23:43 -0300

    atualização do codigo

---
+++ Blockchain em python/arvore_merkle.py
@@ -0,0 +1,38 @@
+import hashlib
+
+
+def calcular_hash(dado):
+    return hashlib.sha256(dado.encode('utf-8')).hexdigest()
+
+class ArvoreMerkle:
+    def __init__(self, transacoes):
+        self.transacoes = transacoes
+        self.raiz = self.construir_arvore(transacoes)
+
+    def construir_arvore(self, transacoes):
+        if len(transacoes) == 0:
+            return None
+        if len(transacoes) == 1:
+            return calcular_hash(str(transacoes[0]))
+
+        # Calcular o hash das transações
+        hashes = [calcular_hash(str(transacao)) for transacao in transacoes]
+
+        # Construir a árvore de Merkle
+        while len(hashes) > 1:
+            if len(hashes) % 2 != 0:
+                hashes.append(hashes[-1])  # Duplicar o último hash se o número for ímpar
+
+            hashes = [calcular_hash(hashes[i] + hashes[i + 1]) for i in range(0, len(hashes), 2)]
+
+        return hashes[0]
+
+    def verificar_enderecos(self):
+        for transacao in self.transacoes:
+            if not self.endereco_valido(transacao.remetente) or not self.endereco_valido(transacao.destinatario):
+                return False
+        return True
+
+    def endereco_valido(self, endereco):
+        # Verificar se o endereço possui a estrutura desejada (ex: 50 caracteres hexadecimais)
+        return len(endereco) == 50 and endereco[:2] == "2x" and all(c in "0123456789abcdef" for c in endereco[2:])
---
+++ Blockchain em python/main.py
@@ -0,0 +1,36 @@
+from cadeia_de_blocos import CadeiaDeBlocos
+from transacao import Transacao
+
+if __name__ == "__main__":
+    minha_cadeia = CadeiaDeBlocos(dificuldade=2)
+
+   
+    remetente1 = Transacao.gerar_endereco()
+    destinatario1 = Transacao.gerar_endereco()
+    transacao1 = Transacao(remetente1, destinatario1, 50)
+    
+    remetente2 = Transacao.gerar_endereco()
+    destinatario2 = Transacao.gerar_endereco()
+    transacao2 = Transacao(remetente2, destinatario2, 20)
+    
+    remetente3 = Transacao.gerar_endereco()
+    destinatario3 = Transacao.gerar_endereco()
+    transacao3 = Transacao(remetente3, destinatario3, 10)
+
+    # Adicionar blocos com transações
+    minha_cadeia.adicionar_bloco([transacao1, transacao2])
+    minha_cadeia.adicionar_bloco([transacao3])
+
+    
+    minha_cadeia.mostrar_cadeia()
+
+    
+    if minha_cadeia.validar_integridade():
+        print("\n--- Cadeia de blocos válida ---")
+    else:
+        print("\n--- Cadeia de blocos inválida! ---")
+
+    # Mostrar histórico de transações para todos os endereços
+    print("\nHistórico de Transações para todos os endereços:")
+    for endereco in Transacao.historico_transacoes:
+        Transacao.mostrar_historico(endereco)
